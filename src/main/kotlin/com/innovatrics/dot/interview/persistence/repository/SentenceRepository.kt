e6550534f1c4bd845eb78910b08078d4b9326bb9
s.hapughin <s.hapughin@sportradar.com>
2024-10-07 11:42:20 +0200

    feat: add getAllByBookId to SentenceRepository ✨

--- src/main/kotlin/com/innovatrics/dot/interview/persistence/repository/SentenceRepository.kt
+++ src/main/kotlin/com/innovatrics/dot/interview/persistence/repository/SentenceRepository.kt
@@ -2,10 +2,18 @@ package com.innovatrics.dot.interview.persistence.repository
 
 import com.innovatrics.dot.interview.persistence.entity.Sentence
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 
 interface SentenceRepository : JpaRepository<Sentence, String> {
     @Transactional(propagation = Propagation.REQUIRED)
-    fun getAllByBookId(bookId: String)
+    fun getAllByBookId(bookId: String): List<Sentence>
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Query("SELECT s.wordCount FROM Sentence s WHERE s.id = :id")
+    fun findWordCountById(
+        @Param("id") id: String,
+    ): Int?
 }
