7f3d372f309c213336755864720712679546f08b
s.hapughin <s.hapughin@sportradar.com>
2024-10-07 11:40:41 +0200

    feat: enable caching for app ⚡️

--- src/main/kotlin/com/innovatrics/dot/interview/KotlinApplication.kt
+++ src/main/kotlin/com/innovatrics/dot/interview/KotlinApplication.kt
@@ -2,10 +2,13 @@ package com.innovatrics.dot.interview
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class KotlinApplication
 
+@Suppress("SpreadOperator")
 fun main(args: Array<String>) {
     runApplication<KotlinApplication>(*args)
 }
